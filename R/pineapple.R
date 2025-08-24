e41648d7490c94f361a59ae2044346c8ca806a79
Jan Radek <jan.radek@unibas.ch>
2025-08-24 15:03:24 +0200

    Add new fruit

--- R/pineapple.R
+++ R/pineapple.R
@@ -5,7 +5,7 @@
 #'
 #' @examples
 #' pineapple()
-#' "I like pineapples"
+#' "I like pineapples and mangos"
 pineapple <- \() {
-  print("I like pineapples")
+  print("I like pineapples and mangos")
 }
